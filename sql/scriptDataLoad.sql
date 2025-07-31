c35ea50a22f31c7a3676aa0ee035c5896d6d870e
Mariana Marrão <marimarrao0115@gmail.com>
2025-07-31 01:28:58 +0000

    feat(elementos): adicionando o gerador de elementos e o script Dataload da tabela de elementos

--- sql/scriptDataLoad.sql
+++ sql/scriptDataLoad.sql
@@ -108,3 +108,26 @@ VALUES
 ('Batata Assada', '83g', 18),
 ('Caldo Verde', '342ml', 19),
 ('Panetone com Açúcar e Canela', '193g', 20);
+
+-- Inserção de dados na tabela elementos
+INSERT INTO elementos (id_receita, id_ingredientes)
+VALUES(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5),
+(6, 6),
+(7, 7),
+(8, 8),
+(9, 9),
+(10, 10),
+(11, 11),
+(12, 12),
+(13, 13),
+(14, 14),
+(15, 15),
+(16, 16),
+(17, 17),
+(18, 18),
+(19, 19),
+(20, 20);
